8283641be53a3541c409c30404745fe5a39a8543
Luis Andrade <landroide@Luiss-MacBook-Pro.local>
2023-04-19 16:08:25 +1200

    Remove items and check items on list

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
     databaseURL: "https://cat-base-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -25,20 +25,37 @@ button.addEventListener("click", () => {
 });
 
 onValue(shoppingListDB, (snapshot) => {
-    let items = Object.entries(snapshot.val());
-    cleanListEl();
 
-    for(let i = 0; i < items.length; i++){
-        let currentItem = items[i];
-        let currentId = currentItem[0];
-        let currentVal = currentItem[1];
+    if(snapshot.exists()){
+        let items = Object.entries(snapshot.val());
+        cleanListEl();
 
-        addItem(currentVal);
+        for(let i = 0; i < items.length; i++){
+            let currentItem = items[i];
+            let currentId = currentItem[0];
+            let currentVal = currentItem[1];
+            addItem(currentItem);
+        }
+    }else{
+        list.innerHTML = "No items here..yet";
     }
+    
 })
 
 let addItem = item => {
-    list.innerHTML += `<li>${item}</li>`
+
+    let itemId = item[0];
+    let itemVal = item[1];
+
+    let newEl = document.createElement("li");
+    newEl.textContent = itemVal;
+
+    newEl.addEventListener("dblclick", () => {
+        let target = ref(database, `shoppingList/${itemId}`)
+        remove(target);
+    })
+
+    list.append(newEl)
 }
 
 let cleanInput = () => {
